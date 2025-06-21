8efa075dfaa5c0ecad215f97cdea6089d0bd4a29
yysgono <yysgono@gmail.com>
2025-06-21 14:14:39 +0900

    적절한 커밋 메시지

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
-// src/firebase.js
-
+// src/utils/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// 아래 config는 Firebase 콘솔에서 복사한 값으로 채워넣으세요!
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA70eeXqzYcXRhSvlyONxCMCHeZBCi02dE",
+  authDomain: "report-a69f5.firebaseapp.com",
+  projectId: "report-a69f5",
+  storageBucket: "report-a69f5.appspot.com",
+  messagingSenderId: "1093968940288",
+  appId: "1:1093968940288:web:4719a1de6acb03906177e5",
+  measurementId: "G-2H1VBDZPP7"
 };
 
 const app = initializeApp(firebaseConfig);
