6b39225ad0fd415b4725c4de9302a603fe7ccb28
yysgono <yysgono@gmail.com>
2025-08-09 05:51:39 +0900

    적절한 커밋 메시지

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -3,6 +3,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://irfyuvuazhujtlgpkfci.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyZnl1dnVhemh1anRsZ3BrZmNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1NDY0MTAsImV4cCI6MjA2NjEyMjQxMH0.q4s3G9mGnCbX7Urtks6_63XOSD8Ry2_GcmGM1wE7TBE";
+const supabaseKey = "sb_secret_3HGduCuPW6RGiocEhpCRxg_jHGgYGUY";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
