7b7104f217151fe5d05752fc11bd1a88df9f8849
yysgono <yysgono@gmail.com>
2025-08-08 22:08:49 +0900

    적절한 커밋 메시지

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -3,4 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
+console.log("SUPABASE_URL:", supabaseUrl);
+console.log("SUPABASE_KEY:", supabaseKey);
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+window.supabase = supabase;
