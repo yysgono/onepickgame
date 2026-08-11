8fac657b1ab25f235b5982e88954ad27b2c001b5
yysgono <yysgono@gmail.com>
2026-08-11 15:00:01 +0900

    적절한 커밋 메시지

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import { fetchWinnerStatsFromDB } from "../utils";
 import MediaRenderer from "./MediaRenderer";
 import FixedCupSection from "./FixedCupCarousel";
 
-const PAGE_SIZE = 32;
+const PAGE_SIZE = 43;
 
 // 애드센스 클라이언트 ID
 const ADSENSE_CLIENT = "ca-pub-2906270915716379";
@@ -451,6 +451,7 @@ visibleList.map((cup, idx) => {
 {idx === 3 && <AdsenseMid />}
 {idx === 6 && <AdsenseMid />}
 {idx === 11 && <AdsenseMid />}
+{idx === 19 && <AdsenseMid />}
 {idx === 25 && <AdsenseMid />}
 {idx === 28 && <AdsenseMid />}
 {idx === 34 && <AdsenseMid />}
