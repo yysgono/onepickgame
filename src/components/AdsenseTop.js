765b31ade8c204aa433056a59b44a3d0d6732c33
yysgono <yysgono@gmail.com>
2026-09-01 00:51:33 +0900

    적절한 커밋 메시지

--- src/components/AdsenseTop.js
+++ src/components/AdsenseTop.js
@@ -17,21 +17,29 @@ export default function AdsenseTop() {
     return () => clearTimeout(timer);
   }, []);
 
-  return (
-     <div style={{ width: "100%", textAlign: "center", margin: "10px 0 0" }}>
-      <ins
-        ref={adRef}
-        className="adsbygoogle"
-        style={{
-          display: "block",
-          width: "728px",   // ⭐ 고정으로 바꿔
-          height: "90px",   // ⭐ 명시해야 뜸
-          margin: "0 auto"
-        }}
-        data-ad-client="ca-pub-2906270915716379"
-        data-ad-slot="3294216783"
-        data-ad-format="horizontal"   // ⭐ auto 금지
-      />
-    </div>
-  );
+return (
+  <div
+    style={{
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      margin: "10px 0 0"
+    }}
+  >
+    <ins
+      ref={adRef}
+      className="adsbygoogle"
+      style={{
+        display: "block",
+        width: "728px",
+        height: "90px",
+        marginLeft: "auto",
+        marginRight: "auto"
+      }}
+      data-ad-client="ca-pub-2906270915716379"
+      data-ad-slot="3294216783"
+      data-ad-format="horizontal"
+    />
+  </div>
+);
 }
